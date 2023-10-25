0edce45ddb1390d5d9fcb8f0b7fd8e14bfe54710
antonio-petrillo <antonio.petrillo4@studenti.unina.it>
2023-10-25 10:30:56 +0200

    add exercises in clojure

---
+++ lua/nucleotide-count/nucleotide-count.lua
@@ -0,0 +1,17 @@
+local DNA = { nucleotideCounts = {A = 0, T = 0, C = 0, G = 0} }
+
+function DNA:new(input)
+  self.nucleotideCounts = {A = 0, T = 0, C = 0, G = 0}
+  for char in input:gmatch(".") do
+    local tmp = nucleotideCounts[char]
+    if tmp == nil then
+      error("Invalid Sequence")
+    end
+    nucleotideCounts[char] = tmp + 1
+  end
+end
+
+x = DNA:new("")
+print(x.nucleotideCounts)
+
+return DNA
